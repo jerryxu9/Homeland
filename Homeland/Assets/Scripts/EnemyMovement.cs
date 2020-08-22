634fe0f33d82fb685a8fc57c5619b34603f4be9b
Jerry Xu <54859894+jerryxu9@users.noreply.github.com>
2020-08-22 12:51:57 -0700

    Working on ememy spawning
    Created the game platform, enemy movement prototype. Working on enemy spawning.

---
+++ Homeland/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    public float speed = 10f;
+    private float distance_range = 0.4f;
+    private Transform target;
+    private int waypointIndex = 0;
+
+    public void Start()
+    {
+        target = Waypoints.waypoints[0];
+    }
+
+    // Make the enemy move towards the next waypoint
+    public void Update()
+    {
+        Vector3 direction = target.position - this.transform.position;
+        this.transform.Translate(direction.normalized * speed * Time.deltaTime, Space.World);
+        if (Vector3.Distance(this.transform.position, target.position) <= distance_range)
+        {
+            findNextWaypoint();
+        }           
+    }
+
+    private void findNextWaypoint()
+    {
+        if (waypointIndex >= Waypoints.waypoints.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        waypointIndex++;
+        target = Waypoints.waypoints[waypointIndex];
+    }
+}
---
+++ Homeland/Assets/Scripts/Waypoints.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    // Array of the positions of the waypoints
+    public static Transform[] waypoints;
+
+    // Instantiate waypoints 
+    public void Awake()
+    {
+        waypoints = new Transform[this.transform.childCount];
+        for (int i = 0; i < waypoints.Length; i++)
+        {
+            waypoints[i] = this.transform.GetChild(i);
+        }
+    }
+
+}
